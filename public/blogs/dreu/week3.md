ee44405f01cee7abe006904651c791c00b4543a8
Nabil <nkoney14@outlook.com>
2023-08-18 20:38:56 -0400

    Fixed dates

--- public/blogs/dreu/week3.md
+++ public/blogs/dreu/week3.md
@@ -1,1 +1,5 @@
-TBD
+During my third week I learned how different academic reading was compared to recreational reading.
+
+As I continued to sort through dense papers I began to formulate my own reading strategy to move through the papers quicker. Alongside Dr. Taylor, I was also assigned a graduate student to be my mentor, Tauhid. Tauhid gave me an important piece of advice: when entering a new field nothing will make sense no matter how knowledgable you are.
+
+I realized that the more papers I read the better I would understand the foundational concepts that tied them together. I continued to learn about group activity recognition in this manner and became much more efficient.
--- src/data/dreu.js
+++ src/data/dreu.js
@@ -1,53 +1,52 @@
 const posts = [
 	{
 		title: 'Week 1',
-		date: new Date(2023, 6, 9),
+		date: new Date(2023, 5, 9),
 		post: './blogs/dreu/week1.md',
 	},
 	{
 		title: 'Week 2',
-		date: new Date(2023, 6, 16),
+		date: new Date(2023, 5, 16),
 		post: './blogs/dreu/week2.md',
 	},
 	{
 		title: 'Week 3',
-		date: new Date(2023, 6, 23),
-		summary: 'Relearning how to read',
+		date: new Date(2023, 5, 23),
 		post: './blogs/dreu/week3.md',
 	},
 	{
 		title: 'Week 4',
-		date: new Date(2023, 6, 30),
+		date: new Date(2023, 5, 30),
 		post: './blogs/dreu/week4.md',
 	},
 	{
 		title: 'Week 5',
-		date: new Date(2023, 7, 7),
+		date: new Date(2023, 6, 7),
 		post: './blogs/dreu/week5.md',
 	},
 	{
 		title: 'Week 6',
-		date: new Date(2023, 7, 14),
+		date: new Date(2023, 6, 14),
 		post: './blogs/dreu/week6.md',
 	},
 	{
 		title: 'Week 7',
-		date: new Date(2023, 7, 21),
+		date: new Date(2023, 6, 21),
 		post: './blogs/dreu/week7.md',
 	},
 	{
 		title: 'Week 8',
-		date: new Date(2023, 7, 28),
+		date: new Date(2023, 6, 28),
 		post: './blogs/dreu/week8.md',
 	},
 	{
 		title: 'Week 9',
-		date: new Date(2023, 8, 4),
+		date: new Date(2023, 7, 4),
 		post: './blogs/dreu/week9.md',
 	},
 	{
 		title: 'Week 10',
-		date: new Date(2023, 8, 11),
+		date: new Date(2023, 7, 11),
 		post: './blogs/dreu/week10.md',
 	},
 ];
